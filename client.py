77fa8e12ad3952f497d8ab6f7a3336d42ad2d553
mini_box <39405619+minibox161@users.noreply.github.com>
2018-05-21 21:15:15 +0300

    Add files via upload

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket, pickle
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect(('127.0.0.1', 4000))
+nickname=input("Никнейм:")
+while True:
+    data = nickname,":",input("Напишите сообшение:")
+    data = pickle.dumps(data)
+    server.send(data)
+    data = server.recv(1024)
+    data = pickle.loads(data)
+    print(data[0][0],data[0][1],data[1],data[2])
+
+server.close()
